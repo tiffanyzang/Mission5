ae31faf84cacba3242f275954885d22097d986fc
tiffanyzang <tz252@byu.edu>
2024-02-06 18:32:10 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿$("#btnSubmit").click(function () {
+    alert("The total cost for " + $("#hours").val() +
+        " hours of tutoring will be: $" + ($("#hours").val() * $("#cost").val()) +
+        ".\nWe hope to work with you soon!")
+})
+
+$("[type='number']").keypress(function (evt) {
+    evt.preventDefault();
+});
