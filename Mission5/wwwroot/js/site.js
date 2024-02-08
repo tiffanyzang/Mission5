2d193c065f7bb5ad73f4ada4fe8650854a4dc98e
tiffanyzang <tz252@byu.edu>
2024-02-08 13:39:42 -0700

    comments and finishing touches

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -1,5 +1,11 @@
-﻿$("#btnSubmit").click(function () {
+﻿//functionality for displaying calcuation
+$("#btnSubmit").click(function () {
     $("p").html("The total cost for " + $("#hours").val() +
         " hours of tutoring will be: $" + ($("#hours").val() * $("#cost").val()) +
         ".\nWe hope to work with you soon!")
 })
+
+//disable typing into input box - no negatives allowed
+$("[type='number']").keypress(function (evt) {
+    evt.preventDefault();
+});
